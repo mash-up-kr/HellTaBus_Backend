ec08cd6953a63c71dc155e4ef78f6fdadd220e02
pengin7384 <pengin7384@gmail.com>
2021-10-03 02:46:53 +0900

    Fix :: error

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,4 +1,14 @@
-import {Controller, Get, Post, Body, Param, Delete, Req, Query} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Req,
+  Query,
+  ParseArrayPipe, Patch,
+} from '@nestjs/common';
 import {ExerciseService} from './exercise.service';
 import {CreateExerciseDto} from './dto/create-exercise.dto';
 import {UpdateExerciseDto} from './dto/update-exercise.dto';
