60676d253e1b602fe5f0b36ce31972daa58453c6
Haron <66551410+hoharon@users.noreply.github.com>
2021-10-03 03:36:22 +0900

    update :: exercise findAll API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,33 @@
 import {Controller, Get, Post, Body, Patch} from '@nestjs/common';
 import {UserService} from './user.service';
-import {CreateUserDto} from './dto/create-user.dto';
 import {UpdateUserDto} from './dto/update-user.dto';
+import {ApiDocs} from './user.docs';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @ApiDocs.create('회원가입 API')
+  create(@Body() req) {
+    return this.userService.create(req);
   }
 
-  @Post('login')
-  login() {
-    return null;
-  }
-
-  @Get('my')
-  getMyInfo() {
-    return null;
-  }
-
-  @Patch('my')
-  update(@Body() updateUserDto: UpdateUserDto) {
-    const id = 1;
-    return this.userService.update(+id, updateUserDto);
-  }
+  // @Post('login')
+  // login(@Body req) {
+  //   return {
+  //     accessToken: this.jwtService.sign(payload),
+  //   }
+  // }
+  //
+  // @Get('my')
+  // getMyInfo() {
+  //   return null;
+  // }
+  //
+  // @Patch('my')
+  // update(@Body() updateUserDto: UpdateUserDto) {
+  //   const id = 1;
+  //   return this.userService.update(+id, updateUserDto);
+  // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,63 @@
-import {Injectable} from '@nestjs/common';
-import {CreateUserDto} from './dto/create-user.dto';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {UpdateUserDto} from './dto/update-user.dto';
+import {JwtService} from '@nestjs/jwt';
+import {InjectRepository} from '@nestjs/typeorm';
+import {User} from './entities/user.entity';
+import {Repository} from 'typeorm';
+import {Err} from '../error';
+import {Gender, HealthStyle} from '../constants';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async create(req) {
+    // TODO: req dto 생성
+    const token = await this.jwtService.decode(req.idToken);
+
+    const googleToken = JSON.parse(JSON.stringify(token));
+
+    // TODO: expire 체크
+    if (!googleToken) throw new BadRequestException(Err.TOKEN.INVALID);
+    console.log(googleToken);
+    const existingUser = await this.findUserBySub(googleToken.sub);
+
+    if (existingUser) throw new BadRequestException(Err.USER.EXISTING_USER);
+
+    if (!req.gender.includes(Gender)) {
+      throw new BadRequestException(Err.USER.GENDER_INVALID);
+    }
+
+    if (!req.healthStyle.includes(HealthStyle)) {
+      throw new BadRequestException(Err.USER.HEALTH_STYLE_INVALID);
+    }
+
+    const createdUser = await this.userRepository.save({
+      email: googleToken.email,
+      googleAccount: googleToken.sub,
+      nickname: req.nickname,
+      gender: req.gender,
+      age: req.age,
+      height: req.height,
+      weight: req.weight,
+      healthStyle: req.healthStyle,
+    });
+
+    return createdUser;
+  }
+
+  async findUserBySub(sub: string) {
+    const user = await this.userRepository.findOne({
+      where: {
+        googleAccount: sub,
+      },
+    });
+
+    return user;
   }
 
   findAll() {
