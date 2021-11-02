02136ef1faad1ca33644ceada1d91c1a85f0c9f8
pengin7384 <pengin7384@gmail.com>
2021-11-02 23:47:35 +0900

    update :: user docs

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,41 @@
+import {IsNotEmpty} from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
+import {Gender, HealthStyle} from '../../constants';
+
+export class UserDto {
+  @IsNotEmpty()
+  @ApiProperty()
+  id: number;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  nickname: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  email: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  googleAccount: string;
+
+  @IsNotEmpty()
+  @ApiProperty({enum: Gender})
+  gender: Gender;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  age: number;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  height: number;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  weight: number;
+
+  @IsNotEmpty()
+  @ApiProperty({enum: HealthStyle})
+  healthStyle: HealthStyle;
+}
--- src/user/user.docs.ts
+++ src/user/user.docs.ts
@@ -3,6 +3,10 @@ import {ApiCreatedResponse, ApiOperation, ApiResponse} from '@nestjs/swagger';
 import {UserController} from './user.controller';
 import {GoogleUserDto} from './dto/google-user.dto';
 import {User} from './entities/user.entity';
+import {CreateUserResponseDto} from './dto/create-user-response.dto';
+import {LoginUserResponseDto} from './dto/login-user-response.dto';
+import {GetLoginInfoResponseDto} from './dto/get-login-info-response.dto';
+import {UserDto} from './dto/user.dto';
 
 type SwaggerMethodDoc<T> = {
   [K in keyof T]: (description: string) => MethodDecorator;
@@ -15,12 +19,9 @@ export const ApiDocs: SwaggerMethodDoc<UserController> = {
           summary,
           description: '회원가입을 진행합니다.',
         }),
-        ApiCreatedResponse({
-          description: '사용자 생성',
-          type: GoogleUserDto,
-        }),
         ApiResponse({
           status: 201,
+          type: CreateUserResponseDto,
           description: '사용자 정상 생성',
         }),
         ApiResponse({status: 403, description: 'Forbidden.'}),
@@ -33,12 +34,9 @@ export const ApiDocs: SwaggerMethodDoc<UserController> = {
           summary,
           description: '로그인을 진행합니다.',
         }),
-        ApiCreatedResponse({
-          description: '사용자 로그인',
-          type: GoogleUserDto,
-        }),
         ApiResponse({
           status: 201,
+          type: LoginUserResponseDto,
           description: '사용자 정상 로그인',
         }),
         ApiResponse({status: 403, description: 'Forbidden.'}),
@@ -51,12 +49,9 @@ export const ApiDocs: SwaggerMethodDoc<UserController> = {
           summary,
           description: '현재 로그인한 사용자의 정보를 조회할 수 있습니다.',
         }),
-        ApiCreatedResponse({
-          description: '현재 로그인한 사용자',
-          type: User,
-        }),
         ApiResponse({
           status: 200,
+          type: GetLoginInfoResponseDto,
           description: '정상 조회',
         }),
         ApiResponse({status: 403, description: 'Forbidden.'}),
