3bbfe903410864e6d4ed1f00e4a3925ec06d7069
ujusy <lsyeon1997@ajou.ac.kr>
2021-11-11 02:12:57 +0900

    update :: user docs

--- src/user/dto/update-user-response.dto.ts
+++ src/user/dto/update-user-response.dto.ts
@@ -2,7 +2,7 @@ import {BaseResponseDto} from '../../common/dto/base-response.dto';
 import {ApiProperty} from '@nestjs/swagger';
 import {UpdateUserDto} from './update-user.dto';
 
-export class UpdateBaseUserInformationResponseDto extends BaseResponseDto {
+export class UpdateUserResponseDto extends BaseResponseDto {
   @ApiProperty()
   data: UpdateUserDto;
 }
