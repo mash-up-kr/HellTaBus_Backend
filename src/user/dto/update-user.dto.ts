48208198140223e54191a0a9467fac9e12ca59b9
ujusy <lsyeon1997@ajou.ac.kr>
2021-11-11 02:32:49 +0900

    update :: user update api

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,5 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty} from 'class-validator';
-import {Gender, HealthStyle} from '../../constants';
+import {AudioCoach, Gender, HealthStyle, Speed} from '../../constants';
 
 export class UpdateUserDto {
   @ApiProperty({example: '펭귄', description: '사용자 닉네임'})
@@ -16,4 +15,13 @@ export class UpdateUserDto {
 
   @ApiProperty({example: 'FULL_BODY_WORKOUT', description: '사용자 분할 선택'})
   healthStyle: HealthStyle;
+
+  @ApiProperty({example: 'COMFORTABLE', description: '오디오 코치 선택'})
+  audioCoach: AudioCoach;
+
+  @ApiProperty({example: 'FAST', description: '어떤 속도로 운동'})
+  speed: Speed;
+
+  @ApiProperty({example: 1, description: '설명 필요 여부'})
+  explanation: boolean;
 }
