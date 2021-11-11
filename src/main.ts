b3ed6b4f940b050c434b35f9213659fdb5b8d0a5
unknown <pooh0407@ewhain.net>
2021-11-11 21:39:07 +0900

    create :: response transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import {AppModule} from './app.module';
 import {setupSwagger} from './swagger';
 import {ValidationPipe} from '@nestjs/common';
 import {HttpExceptionFilter} from './httpException.filter';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   setupSwagger(app);
   app.enableCors();
   await app.listen(3000);
