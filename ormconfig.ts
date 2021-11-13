2f4d77bdfbaee0c700d798545737a8d9530b4b69
Haron <66551410+hoharon@users.noreply.github.com>
2021-11-14 00:53:32 +0900

    fix :: db 변경

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,10 +3,10 @@ import * as path from 'path';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'rds-helltabus-an2.cf6jbqhlm3ny.ap-northeast-2.rds.amazonaws.com',
+  host: process.env.DB_HOST || 'localhost',
   port: +process.env.DB_PORT || 3306,
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'root1234',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'hell_ta_bus',
   entities: [path.join(__dirname, '**/*.entity{.ts,.js}')],
   migrations: [__dirname + '/src/migrations/*.ts'],
