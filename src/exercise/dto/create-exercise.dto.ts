b13dd924b92e15505a095782e600d16a60d31cbc
nari1021 <y.shhh000@gmail.com>
2021-12-04 22:41:59 +0900

    update :: create-exercise DTO 수정

--- src/exercise/dto/create-exercise.dto.ts
+++ src/exercise/dto/create-exercise.dto.ts
@@ -50,6 +50,44 @@ export class CreateExerciseDto {
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({example: 'https://www.....', description: '이미지 링크'})
+  @ApiProperty({
+    example:
+      'https://gif.helltabus.com/17601301/17601301-Dumbbell-Goblet-Squat_Thighs-FIX_1080.gif',
+    description: '이미지 링크',
+  })
   imageLink: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example:
+      'Squat는 쪼그리고 앉다는 뜻으로 데드리프트, 벤치프레스와 더불어 웨이트 트레이닝의 3대 운동 중 하나입니다....',
+    description: '어떤 운동인가요?',
+  })
+  what: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '대퇴사두근, 둔근, 척주기립근, 복부 근육, 대퇴근 후면 근육이 집중적으로 운동됩니다...',
+    description: '어느 부위가 운동되나요?',
+  })
+  where: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example:
+      '1. 앉았다 일어나는 동작을 기본으로 하는데 어깨너비로 발을 벌리고 서서 발가락이 전면을 향하도록 선 후,..',
+    description: '운동 방법을 알려주세요.',
+  })
+  how: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '스쿼트는 분명히 프리 웨이트 중에서 최고의 운동효과를 낼 수 있는 운동입니다....',
+    description: '주의 사항이 있나요?',
+  })
+  cautioin: string;
 }
